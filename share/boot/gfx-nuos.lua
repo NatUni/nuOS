93472153591d557d261e77e0427914bbbd3269e2
Yo at Gigachad dot Win <yo@gigachad.win>
2023-04-18 23:35:56 +0000

    boot loader gfx

---
+++ share/boot/gfx-nuos.lua
@@ -0,0 +1,46 @@
+--+-----------------------------------------------------------------------------+
+--| nuOS 0.0.12.99a0                                                     `_v_'  |
+--| Copyright (c) 2008-2022 Chad Jacob Milios and Crop Circle Systems.  {(0,0}  |
+--| All rights reserved.                                               /),,,,)  |
+--+----------------------------------------------------------------------"--"---+
+
+return {
+	brand = {
+		graphic = {
+			"                         _______    ______",
+			"v12.99  [NO WARRANTY]   /  ___  \\  /  ____|",
+			"      ______   __   __ |  |   |  |(  (_",
+			"     |   _  \\ |  | |  ||  |   |  | \\_  \\_",
+			"     |  | |  ||  | |  ||  |   |  |   \\_  \\",
+			"     |  | |  ||  |_|  ||  |___|  | ____)  )",
+			"     |__| |__| \\______| \\_______/ |______/"
+		},
+	image = "/boot/images/nuos-brand.png",
+	},
+	logo = {
+		graphic = {
+			" .  _----_        *      \\`_v_'/",
+			"   /o  0  \\  +       .   {(@,@}",
+			"  (. O . o )    v       /),,,,)",
+			"   \\~ , ' /    /    __----\"--\"--<",
+			"x   `----'    /__--'----<",
+			"     .     _--",
+			"  `      _-",
+			"                   extends",
+			"                FreeBSD 13.2",
+			"",
+			"   the next underground",
+			"              operating system",
+			"",
+			"   of the people,",
+			"         for the people,",
+			"                 by the people",
+			"",
+			"(c) 2023",
+			"    FreeBSD.org, nuOS.org, CCSys.com",
+		},
+		requires_color = true,
+		image = "/boot/images/nuos-mascot.png",
+		image_rl = 19
+	}
+}
