586f4a4eb9c719fc643736e0ff222b3e6a0dd091
Yo at Gigachad dot Win <yo@gigachad.win>
2023-04-20 05:38:19 +0000

    mo betta

--- share/install/activate_gui.sh
+++ share/install/activate_gui.sh
@@ -1,7 +1,24 @@
 sister enable_svc -C "$TRGT" seatd dbus hald webcamd
-if canhas $ADMIN_ACCT; then
-	pw -R "$TRGT" groupmod video -m $ADMIN_ACCT
-fi
-if canhas $USER_ACCT; then
-	pw -R "$TRGT" groupmod video -m $USER_ACCT
-fi
+mkdir -m 1777 "$TRGT/var/run/user"
+cat >> "$TRGT/etc/gettytab" <<'EOF'
+Ly:\
+	:lo=/usr/local/bin/ly:\
+	:al=root:
+EOF
+sed -i '' -e '/^ttyv1[[:space:]]/s/[[:<:]]Pc[[:>:]]/Ly/' "$TRGT/etc/ttys"
+for u in $ADMIN_ACCT $USER_ACCT; do
+	uid=`pw -R "$TRGT" usershow -n $u | cut -d: -f3`
+	pw -R "$TRGT" groupmod video -m $u
+	cat >> "$TRGT/home/$u/.login" <<EOF
+setenv XDG_RUNTIME_DIR /var/run/user/$uid
+/bin/mkdir -p "\${XDG_RUNTIME_DIR}"
+/bin/chflags uunlink "\${XDG_RUNTIME_DIR}"
+/bin/chmod 700 "\${XDG_RUNTIME_DIR}"
+EOF
+	cat >> "$TRGT/home/$u/.profile" <<EOF
+export XDG_RUNTIME_DIR=/var/run/user/$uid
+/bin/mkdir -p "\$XDG_RUNTIME_DIR"
+/bin/chflags uunlink "\$XDG_RUNTIME_DIR"
+/bin/chmod 700 "\$XDG_RUNTIME_DIR"
+EOF
+done
