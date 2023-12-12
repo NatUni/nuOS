e232c9256e5715088c30e2531667c1dd9c71827d
Jedi Hacker <jedi@matrix.gvmnt.net>
2023-12-12 15:49:49 +0000

    mo betta

--- share/examples/install/cargobay_init.sh
+++ share/examples/install/cargobay_init.sh
@@ -1,29 +1,3 @@
-case $NAME in
-	mama)
-		my_ip=23.111.168.34
-		netmask=0xfffffff8
-		defaultrouter=23.111.168.33
-		primary_if=igb0
-	;;
-	willy|hub)
-		my_ip=66.206.20.42
-		netmask=0xfffffff8
-		defaultrouter=66.206.20.41
-		primary_if=igb0
-	;;
-	iron)
-		my_ip=168.235.81.21
-		netmask=0xffffff00
-		defaultrouter=168.235.81.1
-	;;
-	mcfly)
-		my_ip=168.235.71.47
-		netmask=0xffffff00
-		defaultrouter=168.235.71.1
-	;;
-	*) die
-esac
-
 mv "$TRGT/etc/newsyslog.conf" "$TRGT/etc/newsyslog.conf.sample"
 awk '
 	($1 ~ "^/") {
@@ -36,36 +10,18 @@ awk '
 	{ print $0 }
 	' "$TRGT/etc/newsyslog.conf.sample" > "$TRGT/etc/newsyslog.conf"
 
-
-if canhas ${primary_if-}; then
-	set_primary_phys_netif $primary_if "$TRGT"
-fi
-
-sed -i '' -e '/^#/d;/^$/d' "$TRGT/etc/rc.conf.local"
-
 cat >> "$TRGT/etc/rc.conf.local" <<EOF
-ifconfig_net0="inet $my_ip netmask $netmask -rxcsum -rxcsum6 -txcsum -txcsum6 -lro -tso -vlanhwtso"
-defaultrouter="$defaultrouter"
 gateway_enable="YES"
 firewall_nat_enable="YES"
 EOF
 
-sed -i '' -E -e '/^#VersionAddendum\>/a\
-ChallengeResponseAuthentication no\
-PasswordAuthentication no\
-AuthenticationMethods publickey,publickey,publickey
-' "$TRGT/usr/local/etc/ssh/sshd_config"
-
-mv "$TRGT/usr/local/etc/ssh/moduli" "$TRGT/usr/local/etc/ssh/moduli.sample"
-awk '($1 == "#" || $5 > 4000)' "$TRGT/usr/local/etc/ssh/moduli.sample" > "$TRGT/usr/local/etc/ssh/moduli"
-
-sister enable_svc -C "$TRGT" ntpd openssh
+. $NUOS/share/install/keep_time.sh
+. $NUOS/share/install/harden_remote_login.sh
+. $NUOS/share/install/allow_remote_login.sh
+. $NUOS/share/examples/install/enable_static_cargobay_network.sh
 
 case $NAME in
-	willy|mama|hub)
-		cat >> "$TRGT/etc/rc.conf.local" <<EOF
-ifconfig_net0_alias0="inet `next_ip $my_ip` netmask 0xffffffff"
-EOF
+	willy|mama|hub|neo|matrix)
 		cp -v "$NUOS/share/examples/install/cargobay_genesis.sh" "$TRGT/root/"
 		echo 'nuos_firstboot_script="/root/cargobay_genesis.sh"' > "$TRGT/etc/rc.conf.d/nuos_firstboot"
 	;;
