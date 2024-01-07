4bb5787cc174ab7e33cafe6a8f5ddb35e4434a51
Yo at Gigachad dot Win <yo@gigachad.win>
2024-01-07 12:34:21 +0000

    prepare for new server launch: neo/matrix

--- share/examples/install/enable_static_cargobay_network.sh
+++ share/examples/install/enable_static_cargobay_network.sh
@@ -1,31 +1,9 @@
 case $NAME in
-	mama|spore)
-		my_ip=23.111.168.34
-		netmask=0xfffffff8
-		defaultrouter=23.111.168.33
-		primary_if=igb0
-	;;
-	willy|hub)
-		my_ip=66.206.20.42
-		netmask=0xfffffff8
-		defaultrouter=66.206.20.41
-		primary_if=igb0
-	;;
 	neo|matrix)
 		my_ip=209.133.193.154
 		netmask=0xfffffff8
 		defaultrouter=209.133.193.153
 		primary_if=ix0
-	;;
-	iron)
-		my_ip=168.235.81.21
-		netmask=0xffffff00
-		defaultrouter=168.235.81.1
-	;;
-	mcfly)
-		my_ip=168.235.71.47
-		netmask=0xffffff00
-		defaultrouter=168.235.71.1
 	;;
 	*) error 6 "No network configuration found for $NAME"
 esac
@@ -42,7 +20,7 @@ defaultrouter="$defaultrouter"
 EOF
 
 case $NAME in
-	willy|mama|hub|neo|matrix)
+	neo|matrix)
 		cat >> "$TRGT/etc/rc.conf.local" <<EOF
 ifconfig_net0_alias0="inet `next_ip $my_ip` netmask 0xffffffff"
 EOF
