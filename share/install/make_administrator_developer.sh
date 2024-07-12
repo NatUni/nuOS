2ea8f0fa4899af31cf322649f4b91f59bf7ce016
Jedi Hacker <jedi@tty.zion.top>
2024-07-11 21:37:35 -0400

    lil mo betta

--- share/install/make_administrator_developer.sh
+++ share/install/make_administrator_developer.sh
@@ -1,3 +1,7 @@
-for u in $ADMIN_ACCT $BD_ACCT; do
-	[ -d "$TRGT/home/$u/nuOS" ] || cp -av "$NUOS_CODE" "$TRGT/home/$u/nuOS"
-done
+if [ -f "$NUOS_CODE/conf" ]; then
+	warn you are not a developer to begin with
+else
+	for u in $ADMIN_ACCT $BD_ACCT; do
+		[ -d "$TRGT/home/$u/$(basename "$NUOS_CODE")" ] || cp -av "$NUOS_CODE" "$TRGT/home/$u/"
+	done
+fi
